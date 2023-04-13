1c7a1b06c60d5ef508b9526695460c8759c0e747
Jossuel Mendes <jossueltherock@gmail.com>
2023-04-13 17:43:00 -0300

    Refactor Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,85 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:mobileapp/views/combustivel.dart';
-import 'package:mobileapp/views/layoutflutter.dart';
-import 'package:mobileapp/views/imc.dart';
+import 'package:mobileapp/views/telaprincipal.dart';
 
 
 void main() {
   runApp(const MyApp());
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
-  late TabController _tabController;
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(length: 3, vsync: this);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('TabBar Widget'),
-        bottom: TabBar(
-          controller: _tabController,
-          tabs: const <Widget>[
-            Tab(
-              icon: Icon(Icons.calculate),
-            ),
-            Tab(
-              icon: Icon(Icons.view_sidebar),
-            ),
-            Tab(
-              icon: Icon(Icons.directions_car),
-            ),
-          ],
-        ),
-      ),
-      body: TabBarView(
-        controller: _tabController,
-        children: const <Widget>[
-          Center(
-            child: Imc(),
-          ),
-          Center(
-            child: Layout(),
-          ),
-          Center(
-            child: Gasolina(),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-
-
 
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:mobileapp/main.dart';
+import 'package:mobileapp/views/telaprincipal.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
